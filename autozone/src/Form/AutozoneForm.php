4673bd46d1c2a13364992dffcec2d69a5a447191
Rajendra <rajenthakur@yahoo.com>
2023-05-29 00:51:06 +0530

    updated code

--- autozone/src/Form/AutozoneForm.php
+++ autozone/src/Form/AutozoneForm.php
@@ -24,58 +24,6 @@ class AutozoneForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    // $url = "https://vpic.nhtsa.dot.gov/api/vehicles/GetAllMakes?format=json"
-    //     $request = file_get_contents($uri);
-    //     $makes_list = json::decode($request->getBody()->getContents());
-    //     print_r($makes_list);die("test");
-
-
-    //     try{
-
-    //         $client = new Client([
-    //             'headers' => [
-    //                 'Accept' => 'application/json',
-    //                 'Content-Type' => 'application/json',
-    //              ],
-    //             "base_uri"=> $this->base_uri,
-    //             'connect_timeout' => 10
-    //         ]);
-
-    //     $response = $client->request(
-    //                     "GET",
-    //                    "https://vpic.nhtsa.dot.gov/api/vehicles/GetAllMakes?format=json", 
-    //                     );
-    //     $abc = json_decode($response->getBody()->getContents(), true);
-    //     print_r($abc);die("sssssssssssss");
-    // }
-
-    // catch(RequestException $ex){
-    //     $apiResponse = json_decode($response->getBody()->getContents(), true);
-    //     return $apiResponse['message'];
-    // }
-
-
-
-// $url = "https://vpic.nhtsa.dot.gov/api/vehicles/GetAllMakes?format=json&modelyear=2011";
-
-// $curl = curl_init($url);
-// curl_setopt($curl, CURLOPT_URL, $url);
-// curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-// $headers = array(
-//    "Accept: application/json",
-// );
-// curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-// //for debug only!
-// curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-// curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-
-// $resp = curl_exec($curl);
-// curl_close($curl);
-// var_dump($resp);
-
-
-
 
     $opt = static::foodYears();
     
@@ -113,7 +61,7 @@ class AutozoneForm extends FormBase {
         '#type' => 'select',
         '#title' => 'Available Makes',
         '#options' =>static::availableMake($cat),
-        '#default_value' => !empty($form_state->getValue('makes')) ? $form_state->getValue('makes') : '',
+        'default_value' => !empty($form_state->getValue('makes')) ? $form_state->getValue('makes') : '--None--',
         '#prefix' => '<div id="field-container"',
         '#suffix' => '</div>',
         '#ajax' => [
@@ -149,7 +97,7 @@ class AutozoneForm extends FormBase {
   }
 
   public function DropdownCallback(array &$form, FormStateInterface $form_state) {
-    return $form['availablemakes'];
+    return $form['availablemake'];
   }
   public function SecondDropdownCallback(array &$form, FormStateInterface $form_state) {
     return $form['availablemodel'];
@@ -166,26 +114,23 @@ class AutozoneForm extends FormBase {
    return $years;
   }
 
-  public function availablemakes($year) {
+  public function availablemake($year) {
     $url = 'https://vpic.nhtsa.dot.gov/api/vehicles/GetMakesForManufacturerAndYear/mer?year='.$year.'&format=json';
     $data = $this->getJsonContent($url, 'MakeId', 'MakeName');
-    
+    if(empty($data)) {
+        $data = [''=>'-None-'];
+    }
     return $data;
   }
   public function getJsonContent($url, $id, $name) {
     $response = file_get_contents($url);
     $cat_facts = Json::decode($response);
     $data = [];
+    //$data[] = [''=>'-None-'];
     foreach ($cat_facts as  $cat_fact) {
         $i = 0;
         foreach ($cat_fact as $item) {
-            $i++;
-            // print_r($item);die("ssssss");
-            if($i != 50) {
-                $data[$item[$id]] = $item[$name];
-            } else {
-               break;
-            }
+            $data[$item[$id]] = $item[$name];
         }
     }
     
@@ -197,7 +142,9 @@ class AutozoneForm extends FormBase {
     // $data = $this->getJsonContent($url, 'Make_Id', 'Make_Name');
     $url = 'https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeIdYear/makeId/'.$model_id.'/modelyear/'.$year.'?format=json';
     $data = $this->getJsonContent($url, 'Model_ID', 'Model_Name');
-    
+    if(empty($data)) {
+        $data = [''=>'-None-'];
+    }
     return $data;
   }
 
